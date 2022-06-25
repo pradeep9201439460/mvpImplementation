f106a05ca973a9a0cd8d5711f89dfc6e68c242d4
pradeepsingh <p9201439460@gmail.com>
2022-06-25 10:59:04 +0530

    Initial commit

---
+++ app/src/main/java/com/pradeep/mvpImplementation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pradeep.mvpImplementation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
